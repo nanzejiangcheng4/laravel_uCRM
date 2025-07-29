aeadde841134ee853ebcfba5d61e633183563744
nanzejiangcheng4 <nanzejiangcheng4@gmail.com>
2025-07-29 10:10:55 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,14 @@
+<script setup>
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import TextInput from "@/Components/TextInput.vue";
+import { ref } from "vue";
+
+const input = ref("初期値が入る");
+</script>
+<template>
+    <GuestLayout>
+        <InputLabel value="件名" />
+        <TextInput v-model="input" class="w-full" />
+    </GuestLayout>
+</template>
